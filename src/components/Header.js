fdc887244cfdbf99e6236eb1bc09e827eea627ef
animeshanand1 <animeshanand9@gmail.com>
2023-03-27 18:47:13 +0530

    made changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,22 +5,22 @@ function Header() {
   return (
     <div>
       <nav className="bar navbar-expand-lg navbar-dark bg-success">
-        <div className="container-fluid">
-          <Link className="navbar-brand" to="/">GoFood</Link>
+        <div className="container-fluid" style={{display:'flex',textAlign:'end' ,position:'relative'}}>
+          <Link className="navbar-brand" style={{position:'absolute',right:'2rem',top:'0.5rem'}} to="/">GoFood</Link>
           
-          <div className="collapse navbar-collapse" id="navbarNav">
+          <div style={{display:'flex' ,marginLeft:'4px'}} className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
               <li className="nav-item">
                 <Link className="nav-link active" aria-current="page" to="/">
                   Home
                 </Link>
               </li>
-              <li className="nav-item">
+              <li className="nav-item" style={{marginLeft:'10px'}}>
                 <Link className="nav-link" to="/login">
                   Login
                 </Link>
               </li>
-              <li className="nav-item">
+              <li className="nav-item" style={{marginLeft:'10px'}} >
                 <Link className="nav-link" to="/signup">
                   Signup
                 </Link>
